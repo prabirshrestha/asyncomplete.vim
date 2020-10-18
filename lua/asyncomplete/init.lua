156a2cf32243ee7ee09d3b59a3397825032bdccd
Prabir Shrestha <mail@prabir.me>
2020-10-18 20:23:21 +0000

    fix has_lua check

--- lua/asyncomplete/init.lua
+++ lua/asyncomplete/init.lua
@@ -9,7 +9,7 @@ function M.has_lua()
 end
 
 function M.init()
-    if vim and vim.fn and vim.fn.has and vim.fn.has('nvim-0.5.0') or (vim.fn.has('lua') and vim.fn.has('patch-8.2.1066')) == 0 then
+    if vim and vim.fn and vim.fn.has and (vim.fn.has('nvim-0.5.0') or (vim.fn.has('lua') and vim.fn.has('patch-8.2.1066')) == 0) then
         has_lua = true
     else
         has_lua = false
