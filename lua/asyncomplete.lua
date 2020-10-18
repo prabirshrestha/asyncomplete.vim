87deb0fb267fb3bcc5761c9476469260662e95aa
Prabir Shrestha <mail@prabir.me>
2020-10-18 17:35:25 +0000

    add require('asyncomplete').has_lua()

--- lua/asyncomplete.lua
+++ lua/asyncomplete.lua
@@ -1,7 +1,17 @@
 local M = {}
 
+local has_lua = false
+
+function M.has_lua()
+    return has_lua
+end
+
 function M.init()
-    print('init')
+    if vim.fn.has('nvim-0.5.0') or (vim.fn.has('lua') and vim.fn.has('patch-8.2.1066')) == 0 then
+        has_lua = true
+    else
+        has_lua = false
+    end
 end
 
 return M
