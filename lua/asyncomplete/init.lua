8078e87ab72db0f4ec41850a448edb04d856e57c
Prabir Shrestha <mail@prabir.me>
2020-10-18 20:14:42 +0000

    subscribe to InsertEnter, TextChanged, TextChangedI, TextChangedP and InsertLeave appropriately and also cleanup as necessary

--- lua/asyncomplete/init.lua
+++ lua/asyncomplete/init.lua
@@ -1,4 +1,6 @@
 local M = {}
+local C = require 'asyncomplete/callbag'
+local disable = nil
 
 local has_lua = false
 
@@ -11,6 +13,38 @@ function M.init()
         has_lua = true
     else
         has_lua = false
+    end
+
+    M.enable()
+end
+
+function M.enable()
+    if not has_lua then return end
+    if disable then return end
+
+    disable = C.pipe(
+        C.fromEvent('InsertEnter', 'asyncomplete__insertenter'),
+        C.map(function() print('insert enter') end),
+        C.switchMap(function ()
+            return C.pipe(
+                C.fromEvent({ 'TextChanged', 'TextChangedI', 'TextChangedP' }, 'asyncomplete__textchanged'),
+                C.map(function () print('textchanged') end),
+                C.takeUntil(
+                    C.pipe(
+                        C.fromEvent('InsertLeave', 'asyncomplete__insertleave'),
+                        C.map(function() print('insert leave') end)
+                    )
+                )
+            )
+        end),
+        C.subscribe({})
+    )
+end
+
+function M.disable()
+    if not disable then
+        disable()
+        disable = nil
     end
 end
 
