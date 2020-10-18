1379ab6937ef8b3327360aa31ee35cfaa23658d4
Prabir Shrestha <mail@prabir.me>
2020-10-18 23:12:52 +0000

    add register(), get_active_sources_for_buffer() and vimlist_new() and vimlist_insert() methods

--- lua/asyncomplete/init.lua
+++ lua/asyncomplete/init.lua
@@ -2,7 +2,10 @@ local M = {}
 local C = require 'asyncomplete/callbag'
 local disable = nil
 
+local sources = {}
+
 -- TODO: implement lazy()
+local init = false
 local has_lua = false
 local has_nvim = false
 
@@ -14,20 +17,40 @@ function M.has_nvim()
     return has_nvim
 end
 
+local function vimlist_new()
+    error('asyncomplete not initialized')
+end
+
+local function vimlist_insert(list, value)
+    error('asyncomplete not initialized')
+end
+
 function M.init()
+    if init then
+        error('asyncomplete already inited')
+        return
+    end
+
+    init = true
+
     if not (vim and vim.fn and vim.fn.has and (vim.fn.has('nvim-0.5.0') or (vim.fn.has('lua') and vim.fn.has('patch-8.2.1066')) == 0)) then
         has_lua = false
         return
     end
 
     has_lua = true
+    has_nvim = vim.fn.has('nvim') == 1
 
-    if vim.fn.has('nvim') == 1 then
+    if M.has_nvim() then
         M.vimcmd = vim.api.nvim_command
         M.vimeval = vim.api.nvim_eval
+        vimlist_new = function () return {} end
+        vimlist_insert = function (list, value) table.insert(list, value) end
     else
         M.vimcmd = vim.command
         M.vimeval = vim.eval
+        vimlist_new = function () return vim.list() end
+        vimlist_insert = function (list, value) list:add(value) end
     end
 
     if M.is_enabled() then
@@ -51,6 +74,7 @@ function M.enable()
         C.fromEvent('InsertEnter', 'asyncomplete__insertenter'),
         C.filter(function () return M.is_enabled() end),
         C.map(function () print('insert enter') end),
+        C.map(function () M.get_active_sources_for_buffer() end), -- pre-cache active sources before the user starts typing
         C.switchMap(function ()
             return C.pipe(
                 C.fromEvent({ 'TextChanged', 'TextChangedI', 'TextChangedP' }, 'asyncomplete__textchanged'),
@@ -64,12 +88,12 @@ function M.enable()
                 C.map(function () print('textchanged') end)
             )
         end),
-        C.subscribe({})
+        C.subscribe({ error = function () M.disable() end })
     )
 end
 
 function M.disable()
-    if not disable then
+    if disable then
         disable()
         disable = nil
     end
@@ -82,6 +106,43 @@ end
 function M.is_enabled_for_buffer(bufnr)
     if not bufnr then bufnr = vim.fn.bufnr('%') end
     return M.has_lua() and M.vimeval('getbufvar(' .. bufnr ..', "asyncomplete_enable")')
+end
+
+function M.register(options)
+    if sources[options['name']] then
+        error('asyncomplete source with name = "' .. options['name'] .. '" already exists.')
+    else
+        sources[options['name']] = options
+    end
+end
+
+function M.unregister(name)
+    -- TODO
+end
+
+function M.get_active_sources_for_buffer(bufnr)
+    if not bufnr then bufnr = vim.fn.bufnr('%') end
+    local result = vim.fn.getbufvar(bufnr, 'asyncomplete_active_sources')
+    if type(result) == 'string' and result ~= '' then
+        return result
+    end
+
+    result = vimlist_new()
+
+    for k,_ in pairs(sources) do
+        vimlist_insert(result, k)
+    end
+
+    vim.fn.setbufvar(bufnr, 'asyncomplete_active_sources', result)
+
+    return result
+end
+
+function clear_active_sources_for_buffer(bufnr)
+    if not bufnr then bufnr = vim.fn.bufnr('%') end
+    if type(result) ~= 'string' then
+        vim.fn.setbufvar(bufnr, 'asyncomplete_active_sources', '')
+    end
 end
 
 return M
